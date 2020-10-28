39adee221781871f1a1710be4fb3f057cb4da117
TC <ssq16shu@uea.ac.uk>
2020-10-28 17:35:26 +0000

    initial commit. Layout of alarm cardViews and recyclerView complete (but untested). proceeding to implement Java next.

---
+++ app/src/main/java/com/example/alarmproject_v1/MainActivity.java
@@ -0,0 +1,36 @@
+//insert header comment block here
+
+
+package com.example.alarmproject_v1;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.PersistableBundle;
+import android.view.View;
+
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    //todo: add formatting for standard items again after Android studios reinstall eg:
+    //== CONSTANTS ==//
+
+    //== MEMBER VARIABLES ==//
+    //-LAYOUT-//
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+
+
+    }
+}
